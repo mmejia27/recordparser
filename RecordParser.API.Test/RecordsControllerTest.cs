e94be58e74b788dbfdaa294ec588f6d63d59b9a4
Manuel Mejia <me@manuelmejia.com>
2018-03-11 22:15:26 -0500

    Adding API tests

---
+++ RecordParser.API.Test/RecordsControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecordParser.API.Controllers;
+
+namespace RecordParser.API.Test
+{
+    [TestClass]
+    public class RecordsControllerTest
+    {
+        [TestMethod]
+        public void Records_Get()
+        {
+            var controller = new RecordsController();
+            controller.Post("Mike Adder Male Blue 01/01/2000");
+            controller.Post("John Doe Male Black 03/03/1988");
+
+            var result = controller.Get();
+            Assert.AreEqual(result.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Records_GetByGender()
+        {
+            var controller = new RecordsController();
+            controller.Post("Mike Adder Male Blue 01/01/2000");
+            controller.Post("Jane Doe Female Green 03/03/1988");
+
+            var result = controller.GetByGender();
+            Assert.AreEqual(result.First().Gender, "Female");
+        }
+
+        [TestMethod]
+        public void Records_GetByBirthdate()
+        {
+            var controller = new RecordsController();
+            controller.Post("Mike Adder Male Blue 01/01/2000");
+            controller.Post("Jane Doe Female Green 03/03/1988");
+
+            var result = controller.GetByBirthdate();
+            Assert.AreEqual(result.First().LastName, "Doe");
+        }
+
+        [TestMethod]
+        public void Records_GetByName()
+        {
+            var controller = new RecordsController();
+            controller.Post("Mike Adder Male Blue 01/01/2000");
+            controller.Post("Jane Doe Female Green 03/03/1988");
+            controller.Post("John Abb Male Black 05/05/1985");
+
+            var result = controller.GetByName();
+            Assert.AreEqual(result.First().LastName, "Abb");
+        }
+    }
+}
